358cc128a25acfcfc195b0edcadba0ec01c77834
kevahere <kevahere@gmail.com>
2018-10-05 17:23:12 +0300

    added index view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,13 @@
 from flask import render_template, request, redirect,url_for
 from . import main
 from ..requests import get_sources
+
+@main.route('/')
+def index():
+    """View page for the function that returns the index"""
+    general_news = get_sources('general')[0:5]
+
+    message = "Welcome to the news room"
+    title = "Welcome to the ultimate news room"
+
+    return render_template('index.html',general = general_news,)
