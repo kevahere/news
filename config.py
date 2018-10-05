960bb87e5ea273bc79e8df97eafbcc3158610d9d
kevahere <kevahere@gmail.com>
2018-10-05 11:41:54 +0300

    added config

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    """General configuration parent class"""
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+class ProdConfig(Config):
+    """Production configuration child class"""
+    pass
+
+class DevConfig(Config):
+    """Development configuration child class"""
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'prodconfig' :ProdConfig
+}
