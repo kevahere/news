351d3141fdeb65d026fa61e0c160dc22b9fa9463
kevahere <kevahere@gmail.com>
2018-10-05 15:15:37 +0300

    just checking

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Source:
+    """News Source class to define Source Object"""
+    def __init__(self, id, name,description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
