a38724aad711433259351396646ae676e0c9803c
kevahere <kevahere@gmail.com>
2018-10-08 07:05:45 +0300

    defined articles in index view function

--- app/main/views.py
+++ app/main/views.py
@@ -7,17 +7,20 @@ def index():
     """View page for the function that returns the index"""
     general_news = get_sources('general')[0:5]
 
+    articles = get_articles('id')
+    articles = articles[0:5]
+
     message = "Welcome to the news room"
     title = "Welcome to the ultimate news room"
 
-    return render_template('index.html',general = general_news,message = message, title = title)
+    return render_template('index.html',general = general_news,message = message, title = title, articles = articles)
 
 @main.route('/articles/<id>')
 def articles(id):
     '''
        View  page function that returns the details page and its data
        '''
-    name = "melissamalala"
+    name = "kevin-ahere"
     articles = get_articles(id)
 
     return render_template('article.html', articles=articles, name=name, name_source=id)
